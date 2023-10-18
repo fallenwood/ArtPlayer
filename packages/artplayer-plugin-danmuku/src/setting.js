3851a217ecf6f1f2c0a76c2126e73a7d145494f4
fallenwood <fallenwood.y@outlook.com>
2023-10-18 19:56:25 +0800

    fix size type

--- packages/artplayer-plugin-danmuku/src/setting.js
+++ packages/artplayer-plugin-danmuku/src/setting.js
@@ -258,24 +258,24 @@ export default function setting(art, danmuku) {
                     selector: [
                         {
                             html: '极小',
-                            fontSize: '5',
+                            fontSize: 5,
                         },
                         {
                             html: '较小',
-                            fontSize: '9',
+                            fontSize: 9,
                         },
                         {
                             default: true,
                             html: '适中',
-                            fontSize: '12',
+                            fontSize: 12,
                         },
                         {
                             html: '较大',
-                            fontSize: '16',
+                            fontSize: 16,
                         },
                         {
                             html: '极大',
-                            fontSize: '20',
+                            fontSize: 20,
                         },
                     ],
                     onSelect: function (item) {
